efe3a458de0a15c96fd01ea49e2d403bc414dc81
JUles Saliot <julesaliot@gmail.com>
2023-03-06 20:37:35 +0000

    check

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,23 +3,14 @@ class UsersController < ApplicationController
 
   def spotify
     spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
-    # Now you can access user's private data, create playlists and much more
-
-    # Access private data
     spotify_user.country
     spotify_user.email
-
-   #create devise user
-   @user = User.from_omniauth(request.env["omniauth.auth"])
-    # Get user's top played artists and tracks
-    spotify_user.top_artists
-    spotify_user.top_tracks(time_range: 'short_term')
-
-   if @user.persisted?
-     sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
-   else
-    redirect_to root_path
-   end
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+    else
+      redirect_to root_path
+    end
   end
 
   def index
