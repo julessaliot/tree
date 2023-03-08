110180f36e2457c76c65ffe8659e5a6cdb2ad455
sonia pinho <spinho2@gmail.com>
2023-03-08 15:22:06 +0000

    last changes on landing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     home_path
   end
 end
