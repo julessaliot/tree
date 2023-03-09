f774b8402d8adce462140d591c555639bc16c494
sonia pinho <spinho2@gmail.com>
2023-03-09 17:03:30 +0000

    created comments view still styling

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,17 +8,18 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     @comment.user = current_user
-    @comment.post_id = params[:post_id]
+    @post = Post.find(params[:post_id])
+    @comment.post = @post
     if @comment.save
       redirect_to post_path(@comment.post)
     else
-      render :new, status: :unprocessable_entity
+      render 'posts/show', status: :unprocessable_entity
     end
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id)
+    params.require(:comment).permit(:content)
   end
 end
